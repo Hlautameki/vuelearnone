16d71e36ae301200c8a0cba503d5bbda7834f576
Mateusz Mistecki <mateusz.mistecki@gmail.com>
2021-08-21 00:43:23 +0200

    Added eslint configuration file

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+﻿module.exports = {
+    extends: [
+      // add more generic rulesets here, such as:
+      // 'eslint:recommended',
+      'plugin:vue/vue3-recommended',
+      // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
+    ],
+    rules: {
+      // override/add rules settings here, such as:
+      // 'vue/no-unused-vars': 'error'
+       // 'vue/html-quotes': 'off'
+       "vue/max-attributes-per-line": ["error", {
+        "singleline": {
+          "max": 1,
+          "allowFirstLine": true
+        },      
+        "multiline": {
+          "max": 1,
+          "allowFirstLine": true
+        }
+      }]
+    }
+  }
