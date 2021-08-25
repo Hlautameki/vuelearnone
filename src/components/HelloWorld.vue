9768f51c0225732f12069e7be907e28947153c55
Mateusz Mistecki <mateusz.mistecki@gmail.com>
2021-08-25 15:04:25 +0200

    Added stylus

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
+    <p class="red">
+      Forssaa a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org"
          target="_blank"
@@ -97,7 +97,11 @@
   </div>
 </template>
 
+
+
 <script>
+
+import "../styles/imports.styl"
 export default {
   name: "HelloWorld",
   props: {
@@ -107,19 +111,4 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+
